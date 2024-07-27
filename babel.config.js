4e40c1cbb36b0513a715e4a1d294c83c51853f32
Henrique-Alons0 <henriquethomazsilva8@gmail.com>
2024-07-27 02:04:58 -0300

    Resolve dependences Problem

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,19 @@
+// module.exports = {
+//   presets: [
+//     'module:metro-react-native-babel-preset',
+//     '@babel/preset-env',
+//     '@babel/preset-react',
+//     '@babel/preset-typescript',
+//     '@babel/preset-flow'
+//   ],
+//   plugins: [
+//     'react-native-reanimated/plugin',
+//     '@babel/plugin-transform-runtime',
+//     ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+//     ['@babel/plugin-transform-class-properties', { loose: true }],
+//     ['@babel/plugin-transform-private-methods', { loose: true }],
+//   ],
+// };
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-    '@babel/preset-flow'
-  ],
-  plugins: [
-    'react-native-reanimated/plugin',
-    '@babel/plugin-transform-runtime',
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
